6f15cc2a25e06e0a48beb05724a4264e6a31832b
Sophia Yakovleva <63459167+toosophka@users.noreply.github.com>
2020-04-25 18:04:08 +0700

    Add files via upload

---
+++ vchmat3/vchmat3/Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿using System;
+
+namespace vchmat3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.textBox15 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox16 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(599, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ввести";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(13, 13);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.LabelForeColor = System.Drawing.Color.BlanchedAlmond;
+            series1.Legend = "Legend1";
+            series1.LegendText = "y(x)";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(554, 336);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(599, 29);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(75, 20);
+            this.textBox5.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(596, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Введите количество аргументов xi:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(596, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Табличная функция:";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(596, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "x( i )";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(596, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "f( i ) ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(599, 313);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 36);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Метод\r\nнаименьших квадратов";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(629, 144);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(49, 20);
+            this.textBox1.TabIndex = 14;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(629, 113);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(49, 20);
+            this.textBox2.TabIndex = 15;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(684, 113);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(49, 20);
+            this.textBox3.TabIndex = 16;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(739, 113);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(49, 20);
+            this.textBox4.TabIndex = 17;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(794, 113);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(49, 20);
+            this.textBox6.TabIndex = 18;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(849, 113);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(49, 20);
+            this.textBox7.TabIndex = 19;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(904, 113);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(49, 20);
+            this.textBox8.TabIndex = 20;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(959, 113);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(49, 20);
+            this.textBox9.TabIndex = 21;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(959, 144);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(49, 20);
+            this.textBox10.TabIndex = 27;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(904, 144);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(49, 20);
+            this.textBox11.TabIndex = 26;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(849, 144);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(49, 20);
+            this.textBox12.TabIndex = 25;
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(794, 144);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(49, 20);
+            this.textBox13.TabIndex = 24;
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(739, 144);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(49, 20);
+            this.textBox14.TabIndex = 23;
+            // 
+            // textBox15
+            // 
+            this.textBox15.Location = new System.Drawing.Point(684, 144);
+            this.textBox15.Name = "textBox15";
+            this.textBox15.Size = new System.Drawing.Size(49, 20);
+            this.textBox15.TabIndex = 22;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(739, 313);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(134, 36);
+            this.button3.TabIndex = 29;
+            this.button3.Text = "Формула Лагранжа";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(879, 313);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(129, 36);
+            this.button4.TabIndex = 30;
+            this.button4.Text = "Формула Ньютона";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox16
+            // 
+            this.textBox16.Location = new System.Drawing.Point(599, 287);
+            this.textBox16.Name = "textBox16";
+            this.textBox16.Size = new System.Drawing.Size(79, 20);
+            this.textBox16.TabIndex = 32;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(598, 254);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 30);
+            this.label5.TabIndex = 33;
+            this.label5.Text = "Степень многочлена:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(1034, 366);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox16);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.textBox15);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.TextBox textBox15;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox16;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ vchmat3/vchmat3/Form1.cs
@@ -0,0 +1,385 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace vchmat3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int n;
+        TextBox[] X = new TextBox[7];  //массив ячеек x
+        TextBox[] F = new TextBox[7];  //массив ячеек f
+        double step = 1;
+        
+        //создание массива с данными из текстбокса
+        private double[] array(TextBox[] box, int n)
+        {
+            double[] x = new double[n];
+            double[] f = new double[n];
+
+            //заполняем массив x, f из таблицы
+            for (int i = 0; i < n; i++)
+            {
+                x[i] = new double();
+                x[i] = double.Parse(box[i].Text);
+            }
+            return x;
+        }
+
+        //cтирает данные
+        private void cleaning(int n)
+        {
+            for(int i=0;i<n;i++)
+            {
+                X[i].Clear();
+                F[i].Clear();
+            }
+            chart1.Series[0].Points.Clear();
+            textBox16.Clear();
+        }
+
+        //проверка, что не все х=0, k!=0 
+        private bool check(TextBox[] box, int n)
+        {
+            int fl = 0;
+            for (int i = 0; i < n; i++)
+                if (double.Parse(box[i].Text) == 0)
+                    fl++;
+            if (fl == n)
+            {
+                MessageBox.Show("Невозможно построить график. Введите другие значения х.");
+                return false;
+            }
+            else
+                return true;     
+        }
+
+        //кнопка ввести
+        //пользователь вводит размер таблицы, делает лишние ячейки(максимум ячеек установим 7) невидимыми
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cleaning(n);
+            n = int.Parse(textBox5.Text);
+            if ((int.Parse(textBox5.Text) < 1) || (int.Parse(textBox5.Text) > 7))
+            {
+                MessageBox.Show("Невозможно построить график. Введите n от 1 до 7.");
+                return;
+            }
+            X[0] = textBox2;
+            X[1] = textBox3;
+            X[2] = textBox4;
+            X[3] = textBox6;
+            X[4] = textBox7;
+            X[5] = textBox8;
+            X[6] = textBox9;
+
+            F[0] = textBox1;
+            F[1] = textBox15;
+            F[2] = textBox14;
+            F[3] = textBox13;
+            F[4] = textBox12;
+            F[5] = textBox11;
+            F[6] = textBox10;
+
+            for (int i = 0; i < 7; i++)
+            {
+                if (i < n)
+                {
+                    X[i].Visible = true;
+                    F[i].Visible = true;
+                }
+                else
+                {
+                    X[i].Visible = false;
+                    F[i].Visible = false;
+                }
+                X[i].Clear();
+                F[i].Clear();
+            }
+            
+        }
+
+        //метод Гаусса без выбора главного элемента
+        private double[] gauss(double[,] matrix, double[] y, int n)
+        {
+            double[] x;
+            x = new double[n];
+
+            // переставим строки так, чтобы диагоналные элементы были не 0
+            for (int ind = 0; ind < n; ind++)
+            {
+                int numb = ind;
+                for (int i = 1; i < n; i++)
+                    if (matrix[i,ind] != 0)
+                        numb = i;
+
+                //перестановка строк,ставим на позицию ind строку, в которой ind элемент max
+                if (numb != ind)
+                {
+                    //идем по строке
+                    for (int i = 0; i < n; i++)
+                    {
+                        double tempp = matrix[ind,i];
+                        matrix[ind,i] = matrix[numb,i];
+                        matrix[numb,i] = tempp;
+                    }
+
+                    double temp = y[ind];
+                    y[ind] = y[numb];
+                    y[numb] = temp;
+                }
+            }
+
+            //идем по переменным
+            for (int ind = 0; ind < n; ind++)
+            {
+                //приведем расширенную матрицу к ступенчатому виду
+                //идем по строкам,начиная со следующей после выбранной
+                for (int i = ind + 1; i < n; i++)
+                {
+                    double mult = -matrix[i,ind] / matrix[ind,ind];
+                    if (matrix[i,ind] != 0)
+                    {
+                        for (int j = ind; j < n; j++)
+                            matrix[i,j] += matrix[ind,j] * mult;
+                    }
+                    else
+                        continue;
+                    y[i] += y[ind] * mult;
+                }
+            }
+
+            //обратная подстановка
+            for (int i = n - 1; i >= 0; i--)
+            {
+                x[i] = y[i] / matrix[i,i];
+                for (int j = 0; j < i; j++)
+                    y[j] = y[j] - matrix[j,i] * x[i];
+            }
+
+            return x;
+        }
+
+        //построение графика, настраиваются оси
+        private void graph(double xMin,double xMax,double step,double[] x, double[] y)
+        {
+            chart1.ChartAreas[0].AxisX.Minimum = xMin;
+            chart1.ChartAreas[0].AxisX.Maximum = xMax;
+            //chart1.ChartAreas[0].AxisY.Minimum = 0;
+           // chart1.ChartAreas[0].AxisY.Maximum = 10;
+            chart1.ChartAreas[0].AxisX.MajorGrid.Interval = step;
+            chart1.Series[0].Points.DataBindXY(x, y);
+        }
+        
+        //метод наименших квадратов
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (check(X, n) == false)
+            {
+                cleaning(n);
+                return;
+            }
+            else
+                 if (int.Parse(textBox16.Text) == 0)
+            {
+                MessageBox.Show("Невозможно построить график. Степень многочлена не должна равняться 0.");
+                cleaning(n);
+                return;
+            }
+
+            //заполняем массивы из текстбоксов
+            double[] x = array(X, n);
+            double[] f = array(F, n);
+
+            int k = int.Parse(textBox16.Text); //степень многочлена
+            double[] a = new double[k+1];
+            double[,] c = new double[k + 1, k + 1];
+            double[] d = new double[k + 1];
+
+            int m = 2 * k;
+            double sum;
+            double[] C = new double[m+1];
+
+            if (k == 0)
+            {
+                MessageBox.Show("Невозможно построить график. Степень многочлена не должна равняться 0.");
+                return;
+            }
+
+            //считаем с
+            for(int i=0;i<=m;i++)
+            {
+                C[i] = new double();
+                sum = 0;
+                for (int j = 0; j < n; j++)
+                    sum += Math.Pow(x[j], i);
+                C[i] = sum;
+            }
+
+            //записываем двумерный массив с для решения СЛАУ, 
+            int temp;
+            for(int i=0;i<k+1;i++)
+            {
+                temp = i;
+                for (int j=0;j<k+1;j++)
+                {
+                    c[i, j] = new double();
+                    c[i, j] = C[temp];
+                    temp++;
+                }
+            }
+
+            //считаем d и заполняем массив
+            for (int i = 0; i < k + 1; i++)
+            {
+                sum = 0;
+                d[i] = new double();
+                for (int j = 0; j < n; j++)
+                    sum += f[j] * Math.Pow(x[j], i);
+                d[i] = sum;
+            }
+
+            //решим СЛАУ методом Гаусса и получим коэф. аппроксимирующего многочлена
+            a = gauss(c, d, k + 1);
+
+            //строим график
+            double xMin = x[0]*5;
+            double xMax = x[n-1]*5;
+
+            int count = (int)Math.Ceiling((xMax - xMin) / step)+1;
+            double[] abs = new double[count];
+            double[] ord = new double[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                abs[i] = xMin + step * i;
+                for (int j = 0; j <= k; j++)
+                    ord[i] += Math.Pow(abs[i], j) * a[j];
+            }
+            graph(xMin, xMax, step, abs, ord);
+        }
+
+        //метод Лагранжа
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (check(X, n) == false)
+            {
+                cleaning(n);
+                return;
+            }
+
+            //заполняем массивы из текстбоксов
+            double[] x = array(X, n);
+            double[] f = array(F, n);
+
+            //строим график
+            double xMin = x[0] * 5;
+            double xMax = x[n - 1] * 5;
+
+            int count = (int)Math.Ceiling((xMax - xMin) / step) + 1;
+            double[] abs = new double[count];
+            double[] ord = new double[count];
+
+            double L;
+            double num, den; //числитель и знаменатель для вычисления многочлена в форме Лагранжа
+            for (int i = 0; i < count; i++)
+            {
+                abs[i] = xMin + step * i;
+                L = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    num = 1;
+                    den = 1;
+                    for (int ii = 0; ii < n; ii++)
+                    {
+                        if (ii != j)
+                        {
+                            num *= abs[i] - x[ii];
+                            den *= x[j] - x[ii];
+                        }
+                        else
+                            continue;
+                    }
+                    L += f[j] * num / den;
+                }
+                ord[i] = L;
+            }
+            graph(xMin, xMax, step, abs, ord);
+        }
+
+        //метод Ньютона
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (check(X, n) == false)
+            {
+                cleaning(n);
+                return;
+            }
+            //заполняем массивы из текстбоксов
+            double[] x = array(X, n);
+            double[] f = array(F, n);
+
+            //строим график
+            double xMin = x[0] * 5;
+            double xMax = x[n - 1] * 5;
+
+            int count = (int)Math.Ceiling((xMax - xMin) / step) + 1;
+            double[] abs = new double[count];
+            double[] ord = new double[count];
+
+            double[,] ff = new double[n, n];
+
+            for (int i = 0; i < n; i++)
+                ff[0, i] = f[i];
+
+            int k = n-1;
+
+            //разделенные разности порядка i
+            for (int i = 1; i < n; i++)
+            {
+                int a = 0;
+                for (int j=0;j<k;j++)
+                {
+                    ff[i, j] = (ff[i-1, j + 1] - ff[i-1,j]) / (x[i+a] - x[j]);
+                    a++;
+                }
+                k--;
+            }
+
+            //строим график
+            double P;
+            double mult;
+            for (int i = 0; i < count; i++)
+            {
+                P = ff[0, 0];
+                abs[i] = xMin + step * i;
+                for(int j=1;j<n;j++)
+                {
+                    k = j-1;
+                    mult = 1;
+                    while (k >= 0)
+                    {
+                        mult *= abs[i] - x[k];
+                        k--;
+                    }
+                    P += ff[j, 0] * mult;
+                }
+                ord[i] = P;
+            }
+            graph(xMin, xMax, step, abs, ord);
+        }
+    }
+}
